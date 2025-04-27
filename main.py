2e17999939358983454b146e1a4b22181694151a
MatrixShadeYT <mpro9234@gmail.com>
2025-04-27 01:14:33 -0600

    *100

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ nn.layers = [
 value = nn.output(inputs)
 output = []
 for i in range(len(value)):
-    output.append(tokenizer[round(int(value[i]),0)])
+    output.append(tokenizer[round(int(value[i])*100,0)])
     print(f'N{i+1}: {value[i]}')
 print(output)
