cc8f7f61ec99d6c6c5bcbab46a8f6ee1e237d993
MatrixShadeYT <mpro9234@gmail.com>
2025-04-26 23:46:08 -0600

    assdg

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from neural_network import model, Layer_Dense
+import neural_network as nn
 
 inputed = 1
 outputed = 2
-model = model([
-    Layer_Dense(0,5,inputed),
-    Layer_Dense('SM',5,outputed)
-])
+nn.layers = [
+    nn.Layer_Dense(0,5,inputed),
+    nn.Layer_Dense('SM',5,outputed)
+]
 
 value = model.output([1])
 print(value)
--- neural_network.py
+++ neural_network.py
@@ -1,29 +1,14 @@
 from random import randint
 import numpy as np
 
+# SM = np.exp(x-np.max(x,axis=1,keepdims=True)) / np.sum(np.exp(x-np.max(x,axis=1,keepdims=True)),axis=1,keepdims=True)
+# ReLu = np.maximum(0,x)
+# x = np.dot(inputs,weights)+biases
+
+layers = []
+
 def Layer_Dense(activation,inputs,outputs):
     return [
         activation,np.zeros((inputs,1))[0],
         np.random.randn(outputs,inputs)
     ]
-
-# Model Class - [Layer]
-class model:
-    def __init__(self,layers):
-        self.layers = layers
-    def output(self,inputs):
-        inputs = inputs
-        for i in range(len(self.layers)):
-            x = np.transpose(self.layers[i][2])[0]
-            print(f'inputs: {inputs}')
-            print(f'Weights: {x}')
-            print(f'biases: {self.layers[i][1]}\n')
-            x = np.dot(inputs,x)
-            x += self.layers[i][1]
-            '''if self.layers[i][0] == 'ReLu':
-                inputs = np.maximum(0,x)
-            elif self.layers[i][0] == 'SM':
-                inputs = np.exp(x-np.max(x,axis=1,keepdims=True)) / np.sum(np.exp(x-np.max(x,axis=1,keepdims=True)),axis=1,keepdims=True)
-            else:
-                inputs = x'''
-        return inputs
