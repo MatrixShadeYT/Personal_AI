24a9e852be127d408b29f4e22861121691d3ac2e
MatrixShadeYT <mpro9234@gmail.com>
2025-04-27 00:52:15 -0600

    unnessassary

--- neural_network.py
+++ neural_network.py
@@ -1,10 +1,6 @@
 from random import randint
 import numpy as np
 
-# SM = np.exp(x-np.max(x,axis=1,keepdims=True)) / np.sum(np.exp(x-np.max(x,axis=1,keepdims=True)),axis=1,keepdims=True)
-# ReLu = np.maximum(0,x)
-# x = np.dot(inputs,weights)+biases
-
 layers = []
 
 def output(inputs):
