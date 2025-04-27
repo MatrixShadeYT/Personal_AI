7f82b73c892eb6e32b71537bf5a3e90ddb8c583a
MatrixShadeYT <mpro9234@gmail.com>
2025-04-27 00:12:58 -0600

    print

--- neural_network.py
+++ neural_network.py
@@ -10,8 +10,11 @@ layers = []
 def output(inputs):
     inputs = inputs
     for i in range(len(layers)):
+        print(inputs)
         weights = layers[i][2] # [[],[]]
+        print(weights)
         biases = layers[i][1] # []
+        print(f'{biases}\n')
         x = np.dot(inputs,weights)+biases
         if layers[i][0] == 'SM':
             inputs = np.exp(x-np.max(x,axis=1,keepdims=True)) / np.sum(np.exp(x-np.max(x,axis=1,keepdims=True)),axis=1,keepdims=True)
