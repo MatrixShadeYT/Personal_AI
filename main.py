7c7cb71d79404e7c5f08e14bac2cafc5c312c466
MatrixShadeYT <mpro9234@gmail.com>
2025-04-27 18:45:32 -0600

    usr

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import neural_network as nn
 import string
 
 tokenizer = list(' ,.!?*:'+string.ascii_letters)
-chat = input('Shade: ')
+chat = input('USR: ')
 inputs = []
 for i in list(chat):
     inputs.append(tokenizer.index(i))
