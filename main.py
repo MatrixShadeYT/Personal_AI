056ad9beae6efe63cd707a032cf703f3a1027290
MatrixShadeYT <mpro9234@gmail.com>
2025-04-27 18:46:39 -0600

    1 Layer

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ inputs = []
 for i in list(chat):
     inputs.append(tokenizer.index(i))
 nn.layers = [
-    nn.Layer_Dense('ReLu',5,len(inputs)),
-    nn.Layer_Dense('SM',5,5)
+    nn.Layer_Dense('SM',5,len(inputs))
 ]
 
 value = nn.output(inputs)
