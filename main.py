be60aeaa88a3a772d262d370cc075c80993c38f7
MatrixShadeYT <mpro9234@gmail.com>
2025-04-27 19:10:03 -0600

    change

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import neural_network as nn
 import string
 
 tokenizer = list(' ,.!?*:'+string.ascii_letters)
-inputs = 5
-nn.layers = [
-    nn.Layer_Dense('SM',5,inputs)
-]
+inputs = 2
+outputs = 5
+nn.Model([
+    nn.Layer_Dense(act='SM',neurons=outputs,inputs=inputs)
+])
 
-value = nn.output(inputs)
+value = nn.output([1,0])
 print('Output: '+''.join([f'{value[i]} ' for i in range(len(value))]))
