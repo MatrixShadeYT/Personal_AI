f727f66b0496d27163f8cb7edf21ea8cd388902d
MatrixShadeYT <mpro9234@gmail.com>
2025-04-28 18:09:05 +0000

    Print change

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import neural_network as nn
 x = nn.Layer_Dense(act="SM",neurons=2,inputs=2)
 model = nn.Model([x])
 value = model.output([1,0])
-print('Output: '+''.join(
+print('SM: '+''.join(
     [f'{int(round(100*value[i],0))}% ' for i in range(len(value))]
 ))
 x = ['ReLu',x[1],x[2]]
 model = nn.Model([x])
 value = model.output([1,0])
-print('Output: '+''.join(
+print('ReLu: '+''.join(
     [f'{value[i]} ' for i in range(len(value))]
 ))
 x = [0,x[1],x[2]]
