b8a46c42c22a91ee1bb30f1f055b93954e8d4c59
MatrixShadeYT <mpro9234@gmail.com>
2025-04-27 18:55:37 -0600

    format

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import neural_network as nn
 import string
 
 tokenizer = list(' ,.!?*:'+string.ascii_letters)
-chat = input('USR: ')
 inputs = 5
 nn.layers = [
     nn.Layer_Dense('SM',5,inputs)
 ]
 
 value = nn.output(inputs)
-print('Output: '+''.join(value))
+print('Output: '+''.join([f'{value[0]}', f', {value[i+1]}' for i in range(len(value)-1)]))
