9025b92f9f4731a7198f7aaf8d469a363106a8df
MatrixShadeYT <mpro9234@gmail.com>
2025-04-27 18:53:38 -0600

    print output

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ import string
 
 tokenizer = list(' ,.!?*:'+string.ascii_letters)
 chat = input('USR: ')
-inputs = []
-for i in list(chat):
-    inputs.append(tokenizer.index(i))
+inputs = 5
 nn.layers = [
-    nn.Layer_Dense('SM',5,len(inputs))
+    nn.Layer_Dense('SM',5,inputs)
 ]
 
 value = nn.output(inputs)
-print('Bot: "'+''.join([tokenizer[round(int(value[i]),0)] for i in range(len(value))])+'"')
+print('Output: '+''.join(value))
