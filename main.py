1b4348069ff8c7dc6dbca95b69135a7cae02cab8
MatrixShadeYT <mpro9234@gmail.com>
2025-04-29 04:16:43 +0000

    replit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import neural_network as nn
 
-inputs = [2,3]
-model = nn.Model([nn.Layer_Dense("SM",inputs[1],inputs[0])])
+inputs, outputs = 2, 3
+model = nn.Model([nn.Layer_Dense("SM",outputs,inputs)])
 
-model.output([1,0])
+value = model.output([1,0])
+print(value)
