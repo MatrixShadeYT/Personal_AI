e8c860884cf6037fcdf4e6a815cf4df3292b3f90
MatrixShadeYT <mpro9234@gmail.com>
2025-04-29 04:30:14 +0000

    Genetic Algorithm

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import neural_network as nn
 
 inputs, outputs = 2, 3
 model = nn.Model([nn.Layer_Dense("SM",outputs,inputs)])
+data = model.model() # [[act,biases,[weights]]]
 
-value = model.output([1,0])
-print(value)
+class genetic:
+  generations = []
+  def __init__(self,model):
+    self.model = model
+    self.base = model
+  def generation(self,size,rate):
+    hive = []
+    for i in range(size):
+      x = self.model
+
+AI = genetic(model)
